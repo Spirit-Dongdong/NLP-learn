afeadb630a15783b0b04bc8ed8856fc9907ec7b5
Spirit-Dongdong <spirit.dongdong@gmail.com>
2013-09-29 16:42:25 +0800

    trivial

--- src/dongdong/DidUMeanTest.java
+++ src/dongdong/DidUMeanTest.java
@@ -39,37 +39,42 @@ public class DidUMeanTest {
 
 	private static IndexReader pinyinReader;
 	private static IndexSearcher pinyinSearcher;
-	
+
 	private static IndexReader archiveReader;
 	private static IndexSearcher archiveSearcher;
-	
+
 	private static final String ARCHIVE_INDEX = "D:\\archive_rebuild";
-	
-	private static final String BOT_QUERY = "botQuery.txt"; 
+
+	private static final String BOT_QUERY = "botQuery.txt";
 	private static final String RESULT = "result";
-	
-	private static final float PY_SCORE = 0.9f;
-	
+
+	private static final float PY_SCORE = 1f;
+
 	private static Analyzer analyzer = new IKAnalyzer(false);
-	
-	private static final float MIN_SCORE = -12f;
-	
+
+	private static final double MIN_SCORE_2 = -9;
+	private static final double MIN_SCORE_4 = -9;
+
 	/**
-	 * 0：输入为全英文，通过全英文拿到结果
-	 * 1: 输入为全英文，直接通过拼音找到汉字，返回汉字，得分为0.9
-	 * 2： 输入为全英文，通过拼音找不到汉字，进入计算模块，再把得到结果转换成汉字返回.得分为模块得分*0.9
-	 * 3：输入有汉字，把汉字转换成拼音，再把拼音转成汉字返回。得分为0.9。适用同音字输错
+	 * -1:return null
+	 * 
+	 * 1: 输入为全英文，直接通过拼音找到汉字，返回汉字，得分为1
+	 * 2：输入为全英文，通过拼音找不到汉字，进入计算模块，再把得到结果转换成汉字返回.得分为模块得分
+	 * 3：输入有汉字，把汉字转换成拼音，再把拼音转成汉字返回。适用同音字输错
 	 * 4.输入有汉字，转成拼音后找不到对应汉字，直接通过汉字进入计算模块。得分为模块得分
+	 * 5：输入为全英文，通过全英文拿到结果 
 	 */
 	private static int type;
 
 	private static void initReaderSearcher() {
 		if (pinyinReader == null) {
 			try {
-				pinyinReader = IndexReader.open(FSDirectory.open(new File(BuildIndex.INDEX_PATH)));
+				pinyinReader = IndexReader.open(FSDirectory.open(new File(
+						BuildIndex.INDEX_PATH)));
 				pinyinSearcher = new IndexSearcher(pinyinReader);
-				
-				archiveReader = IndexReader.open(FSDirectory.open(new File(ARCHIVE_INDEX)));
+
+				archiveReader = IndexReader.open(FSDirectory.open(new File(
+						ARCHIVE_INDEX)));
 				archiveSearcher = new IndexSearcher(archiveReader);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -107,10 +112,10 @@ public class DidUMeanTest {
 					return maybeChn;
 				}
 				// start = System.currentTimeMillis();
-//				String bestFit = sc.didYouMean(keyword);
+				// String bestFit = sc.didYouMean(keyword);
 				String[] results = sc.didYouMean2(keyword);
 				String bestFit = results[0];
-				
+
 				// end = System.currentTimeMillis();
 				// System.out.println("didYouMean cost " + (end - start) +
 				// "ms");
@@ -148,31 +153,49 @@ public class DidUMeanTest {
 
 		String result = null;
 
-		Query query = buildQuery(keyword);
-		TopDocs docs = archiveSearcher.search(query, null, 1);
-		
-		if (docs.totalHits > 0) {
-//			System.out.println("不用提示" + keyword);
-			return null;
-		}
-		
+//		Query query = buildQuery(keyword);
+//		TopDocs docs = archiveSearcher.search(query, null, 1);
+//
+//		if (docs.totalHits > 0) {
+//			// System.out.println("不用提示" + keyword);
+//			type = -1;
+//			return null;
+//		}
+
 		if (CharUtil.allAscChar(keyword)) {// 不含中文
 			// try to find both in keyword & pinyin
 			String maybeChn = getKeywordByPinyin(keyword);
 			if (maybeChn != null) {
 				type = 1;
-				return new String[]{maybeChn, String.valueOf(PY_SCORE)};//type = 1,直接从拼音获得
+				return new String[] { maybeChn, String.valueOf(PY_SCORE) };// type
+																			// =
+																			// 1,直接从拼音获得
 			}
 			String[] bestFit = sc.didYouMean2(keyword);
 			if (bestFit == null) {
+				type = -1;
 				return null;
-			} 		
+			}
 
 			if ((result = getKeywordByPinyin(bestFit[0])) != null) {
 				double score = PY_SCORE * Double.parseDouble(bestFit[1]);
 				type = 2;
-				return new String[]{result, String.valueOf(score)};//type=2，纠正拼音，通过拼音拿到汉字
+				if (score < MIN_SCORE_2) {
+//					System.out.println("less than " + MIN_SCORE_2
+//							+ ", deprecated ");
+					type = -1;
+					return null;
+				}
+				return new String[] { result, String.valueOf(score) };// type=2，纠正拼音，通过拼音拿到汉字
 			}
+			double score = Double.parseDouble(bestFit[1]);
+			if (score < MIN_SCORE_2) {
+//				System.out.println("less than " + MIN_SCORE_2
+//						+ ", deprecated ");
+				type = -1;
+				return null;
+			}
+			type = 5;
 			return bestFit;
 
 		} else {// has chinese chars
@@ -180,11 +203,12 @@ public class DidUMeanTest {
 			result = getKeywordByPinyin(pinyin);
 			if (result != null) {
 				type = 3;
-				return new String[]{result, String.valueOf(PY_SCORE)};//type=3，通过汉字转拼音，再转汉字拿到
+				return new String[] { result, String.valueOf(PY_SCORE) };// type=3，通过汉字转拼音，再转汉字拿到
 			} else {
 				type = 4;
-				String[] temp =  sc.didYouMean2(keyword);//tpye=4，汉字去计算拿到
-				if (Float.parseFloat(temp[1]) < MIN_SCORE) {
+				String[] temp = sc.didYouMean2(keyword);// tpye=4，汉字去计算拿到
+				if (Double.parseDouble(temp[1]) < MIN_SCORE_4) {
+					type = -1;
 					return null;
 				}
 				return temp;
@@ -202,7 +226,7 @@ public class DidUMeanTest {
 				Document doc = pinyinReader.document(docs.scoreDocs[0].doc);
 				result = doc.get("keyword");
 				// end = System.currentTimeMillis();
-//				 System.out.println("getKeywordByPinyin cost " + (end - start)
+				// System.out.println("getKeywordByPinyin cost " + (end - start)
 				// + "ms");
 				return result;
 			} else {
@@ -237,7 +261,7 @@ public class DidUMeanTest {
 	}
 
 	public static void main(String[] args) throws IOException {
-		
+
 		long start, end;
 		start = System.currentTimeMillis();
 		DidUMeanTest test = new DidUMeanTest();
@@ -254,71 +278,77 @@ public class DidUMeanTest {
 		end = System.currentTimeMillis();
 		System.out.println("read model cost " + (end - start) + "ms");
 
-//		simpleTest();
+		// simpleTest();
 		test();
 	}
-	
+
 	public static void test() throws IOException {
 		long start, end;
 		start = System.currentTimeMillis();
 		long totalCost = 0;
+		int realCount = 0;
 		int count = 0;
 		StringBuilder sb = new StringBuilder();
-String[] queries = TxtUtil.getFileContent(BOT_QUERY).split("\n");
-		
-		
+		String[] queries = TxtUtil.getFileContent(BOT_QUERY).split("\n");
+//		String[] queries = {"wlgcs"};
+
 		for (String t : queries) {
-			count ++;
+			count++;
 			String query = t.split(":")[0];
 			start = System.currentTimeMillis();
 			String[] result = didUMean(query, true);
 			end = System.currentTimeMillis();
 
-			if (result != null) {
+			if (result != null && validResult(query, result[0])) {
+				realCount++;
 				long cost = end - start;
 				totalCost += cost;
 				sb.append("cost " + cost + "ms\n");
-				sb.append(type + "_" + result[1] + " didUMean " + result[0] + " instead of " + query + "\n" +
-						"------------------------------\n");
+				sb.append(type + "_" + result[1] + " didUMean " + result[0]
+						+ " instead of " + query + "\n"
+						+ "------------------------------\n");
 			}
 
-			
 			if (count % 1000 == 0) {
 				System.out.println(count + " completed");
 				TxtUtil.writeToFile(RESULT, sb.toString(), true);
 				sb = new StringBuilder();
 			}
-			
+
 		}
-		
-		TxtUtil.writeToFile(RESULT, "total cost:" + totalCost + ", avg cost " + totalCost/count, true);
+
+		TxtUtil.writeToFile(RESULT, "total cost:" + totalCost + "count:"
+				+ realCount + ", avg cost " + (double)totalCost / realCount, true);
 	}
-	
+
 	public static void simpleTest() throws IOException {
 		long start, end;
 		start = System.currentTimeMillis();
-		
-		String[] tests = {"excl", "北京盘古氏投资有限公司", "gognsi", "jaav", "工司", "gongsii", "工程司"};
+
+		String[] tests = { "excl", "北京盘古氏投资有限公司", "gognsi", "jaav", "工司",
+				"gongsii", "工程司" };
 		for (String t : tests) {
 			String query = t.split(":")[0];
 			start = System.currentTimeMillis();
 			String[] result = didUMean(query, true);
 			end = System.currentTimeMillis();
 			long cost = end - start;
-			
-			if (result != null) {
+
+			if (result != null && validResult(t, result[0])) {
 				System.out.println("cost " + cost + "ms\n");
-				System.out.println(type + "_" + result[1] + " did u mean " + result[0] + " instead of " + query + "\n" +
-						"------------------------------\n");
+				System.out.println(type + "_" + result[1] + " did u mean "
+						+ result[0] + " instead of " + query + "\n"
+						+ "------------------------------\n");
 			}
 		}
-		
+
 	}
-	
+
 	private static String getAnalyzeResult(String keyword) {
 		StringBuilder sb = new StringBuilder();
 		try {
-			TokenStream ts = analyzer.tokenStream(null, new StringReader(keyword));
+			TokenStream ts = analyzer.tokenStream(null, new StringReader(
+					keyword));
 			while (ts.incrementToken()) {
 				sb.append(ts.getAttribute(CharTermAttribute.class)).append(" ");
 			}
@@ -328,7 +358,7 @@ String[] queries = TxtUtil.getFileContent(BOT_QUERY).split("\n");
 		}
 		return sb.toString();
 	}
-	
+
 	private static Query buildQuery(String keyword) {
 		BooleanQuery query = new BooleanQuery();
 		TermQuery query1 = new TermQuery(new Term("name", keyword));
@@ -337,9 +367,16 @@ String[] queries = TxtUtil.getFileContent(BOT_QUERY).split("\n");
 		query.add(query1, Occur.SHOULD);
 		query.add(query2, Occur.SHOULD);
 		query.add(query3, Occur.SHOULD);
-		
+
 		return query;
-		
+
+	}
+	
+	private static boolean validResult(String origin, String didUMean) {
+		if (origin.trim().equals(didUMean.trim())) {
+			return false;
+		}
+		return true;
 	}
 
 }
