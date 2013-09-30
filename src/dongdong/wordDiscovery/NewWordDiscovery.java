d05d8232c4afed79e750dd24245c6a890a8f8080
Spirit-Dongdong <spirit.dongdong@gmail.com>
2013-09-30 16:45:51 +0800

    新词发现

---
+++ src/dongdong/wordDiscovery/NewWordDiscovery.java
@@ -0,0 +1,19 @@
+package dongdong.wordDiscovery;
+
+public class NewWordDiscovery {
+
+	public static final String JD1 = "resource/intro";
+	public static final String JD2 = "resource/jd1000.csv";
+	
+	public static void init() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
